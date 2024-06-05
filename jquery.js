92c4570bea25eb3a51bc9d5b27175b5246ab4e1d
Prof Edir <ecaramba@gmail.com>
2024-06-04 21:47:06 -0300

    form

--- jquery.js
+++ jquery.js
@@ -5,6 +5,29 @@ jQuery(document).ready(function(){
     {
         var cor = $("#cor").val();
         $(this).css("color", cor);
-    });
+    }); // fim do h3
+
+
+    $(".bt").click(function(){
+
+        var valor = $("#nome").val();
+        var sexo = $("#sexo").val();
+
+        $(".campo input, .campo select").removeClass("erro");
+
+
+        if (valor.trim().length < 3)
+        {
+            alert("O campo nome é obrigatório");
+            $("#nome").addClass("erro");
+        }
+
+        if (sexo == '')
+        {
+            alert("O campo sexo é obrigatório");
+            $("#sexo").addClass("erro");
+        }
+
+    }); // fim do .bt
 
 });
