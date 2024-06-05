c90dcae36ece42b8dac63e2280280d289d0bf658
Prof Edir <ecaramba@gmail.com>
2024-06-05 20:39:13 -0300

    jogo

--- jogovelha.js
+++ jogovelha.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
     
+    var jogador = 1;
+
     $(".casa").click(function(){
-        $(this).html("X");
+        if (jogador == 1)
+        {
+            $(this).html("X");
+            jogador = 2;
+        } else {
+            $(this).html("O");
+            jogador = 1;
+        }
     });
 
 });
